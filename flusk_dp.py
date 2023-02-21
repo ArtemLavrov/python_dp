daa8347ad16fb7bf5fafdf5b63bfafe77f5addb1
ArtemLavrov <artiomlavrov68@gmail.com>
2023-02-21 04:16:34 +0300

    Добавление регистрации(пока что сессионной), подключил CSS, добавил пару тэмплэйтов.

--- flusk_dp.py
+++ flusk_dp.py
@@ -2,7 +2,7 @@ import os
 from datetime import datetime
 
 
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request,send_from_directory,session, redirect,abort
 from flask_sqlalchemy import SQLAlchemy
 # from flask_wtf import FlaskForm
 # from wtforms import FileField, SubmitField
@@ -18,32 +18,50 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 #Создание экземлпяра объекта
 app = Flask(__name__)
+menu = ["Авторизация", "Полезная информация", "Обратная связь"]
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-# app.config['SECRET_KEY'] = 'supersecretkey'
+app.config['SECRET_KEY'] = 'fjehqjchekcejai4kfjkae'
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///D:/python_dp/db.sqlite3'
-# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
+# app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///D:/python_dp/db.sqlite3'
+# # app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# db = SQLAlchemy(app)
 
 
 # class UploadFileForm(FlaskForm):
 #     file = FileField("File", validators=[InputRequired()])
 #     submit = SubmitField("Upload File")
 
+@app.route("/profile/<username>")
+def profile(username):
+    if 'userLogged' not in session or session['userLogged']!=username:
+        abort(401)
+    return render_template('profile.html', title=username, username=username, menu=menu)
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == "Artem" and request.form['psw'] == "1234":
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+    return render_template('login.html', title="Авторизация", menu=menu)
 
 @app.route('/')
 def content():
     crypto_methods = [{'id': 1, 'name_method': 'RSA'},
                       {'id': 2, 'name_method': 'magma'}
                       ]
-    file_loader = FileSystemLoader('templates')
-    env = Environment(loader=file_loader)
-    tm = env.get_template('contents.html')
-    msg = tm.render(methods_name=crypto_methods, domain='http://192.168.0.103:5000', title="Сайтик для шифрования файлов")
-    return msg
+
+    return render_template('contents.html', methods_name=crypto_methods, menu=menu, domain='http://localhost:5000', title="Сайтик для шифрования файлов")
+    #file_loader = FileSystemLoader('templates')
+    #env = Environment(loader=file_loader)
+    # tm = env.get_template('contents.html')
+    # msg = tm.render(methods_name=crypto_methods, domain='http://localhost:5000:5000', title="Сайтик для шифрования файлов")
+    # return msg
 
 
 @app.route('/RSA', methods=['GET', 'POST'])
@@ -55,7 +73,7 @@ def RSA():
             #new_filename = f'{filename.split(".")[0]}' + f'{str(datetime.day)}.' + f'{filename.split(".", 1)[1].lower()}'
             file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'D:/python_dp/Downloads', secure_filename(file.filename)))
         return 'Uploaded'
-    return render_template('RSA.html', domain='http://192.168.0.103:5000/RSA', title="RSA")
+    return render_template('RSA.html', domain='http://localhost:5000/RSA', title="RSA", menu=menu)
     # return render_template('RSA.html', domain='http://192.168.0.103:5000/RSA', title = "RSA", form=form)
 
 
@@ -71,12 +89,15 @@ def magma():
     #     file = form.file.data
     #     file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'], secure_filename(file.filename)))
     #     return ('File has been uploaded')
-    return render_template('magma.html', domain='http://192.168.0.103:5000/magma', title="magma")
+    return render_template('magma.html', domain='http://localhost:5000/magma', title="magma", menu=menu)
 
 
-# @app.route('/download')
-# def download():
-#     return render_template('download.html', files=os.listdir('D:/python_dp/output'))
+@app.route('/download')
+def download():
+    return render_template('download.html', files=os.listdir('D:/python_dp/output'))
+@app.route('/download/<filename>')
+def download_file(filename):
+    return send_from_directory('D:/python_dp/output', filename)
 
 
 
